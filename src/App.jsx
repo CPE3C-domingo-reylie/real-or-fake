ee394ff90628732b06a3d996822f3cf139e277fe
CPE3C-domingo-reylie <2023105858@ms.bulsu.edu.ph>
2026-04-08 14:17:05 +0800

    resultpage

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,16 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/home-page";
 import AboutPage from "./pages/about-page";
 import ContactPage from "./pages/contact-page";
+import ResultsPage from "./pages/results-page";
 
 function App() {
   return (
     <>
-      <ApiTester /> 
+     
       
-      <Routes>
+      <Routes>\
+        <Route path="/apitester" element={<ApiTester/>} />
+        <Route path="/results" element={<ResultsPage/>} />
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/contacts" element={<ContactPage />} />
